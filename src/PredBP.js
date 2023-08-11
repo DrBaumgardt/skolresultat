389980169e16c9495d0c7e0c9c7f377ee8cbb559
DrBaumgardt <baumgardt@gmail.com>
2023-08-11 07:19:02 +0200

    modified:   src/PredBP.js

--- src/PredBP.js
+++ src/PredBP.js
@@ -1,4 +1,4 @@
-// Hej! eeeeeeeeeeeeeeeeeeeeerrr
+// Hej! eeeeeeeeeeeeeeeeeeeeerrr111
 import React, { useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
